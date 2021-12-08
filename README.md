14c8ae66835f0b620c033745aa64bfbad89efdfb
dj-venie <mdj.venie@gmail.com>
2021-12-08 17:24:08 +0900

    finish mAP

--- README.md
+++ README.md
@@ -10,8 +10,15 @@
 - python mAP.py -gt ./path_to_ground_truth -pred ./path_to_prediction
 ### 1.1 mAP
 ![map 사진](./imgs/map_total.jpg)
+
+#### mAP 계산 주의사항
+- score 기준 정렬후 높은 score 먼저 bbox 비교 실행
+- iou가 가장높은 박스를 동시에 예측한경우 스코어가 가장 높은 예측만 tp (repeated match)
+
 ## 2. ocr evaluation
 **Quick start**
 - python ocr_eval.py -gt ./path_to_ground_truth -pred ./path_to_prediction -wem
 ### 2.1 1-NED
 ### 2.2 WEM
+
+
--- mAP.py
+++ mAP.py
@@ -8,6 +8,7 @@ from absl.flags import FLAGS
 #################################################
 flags.DEFINE_string("gt","./mAP_example_files/ground_truth.txt","path to ground truth")
 flags.DEFINE_string("pred","./mAP_example_files/prediction.txt","path to prediction")
+flags.DEFINE_boolean("summary", False,"show only mAP")
 #################################################
 
 """
@@ -35,17 +36,19 @@ def cal_iou(bbox1, bbox2):
     y = max(y1, y2)
     xmax = min(x1+w1, x2+w2)
     ymax = min(y1+h1, y2+h2)
-    intersection_area = (xmax-x) * (ymax-y)
+    
+    if (xmax>x) and (ymax >y):
+        intersection_area = (xmax-x+1) * (ymax-y+1)
+    else:
+        return 0
     if intersection_area < 0:
         # print(f"bbox error {bbox1} or {bbox2}")
         return 0
 
-    box1_area = w1*h1
-    box2_area = w2*h2
+    box1_area = (w1+1)*(h1+1)
+    box2_area = (w2+1)*(h2+1)
     union_area = box1_area + box2_area - intersection_area
-
     return intersection_area / union_area
-
 
 def cal_ap(tp,cls_cnt):
     fp = [0 if i else 1 for i in tp]
@@ -110,8 +113,8 @@ def main(_argv):
         file_dict[name] = [gt_path, pred_dict.get(name,"")]
     # load annotations
 
-    total_dict = {}
     total_tp_dict = {}
+    total_dict = {'gt':{},'pred':{}}
     gt_cls_count = {}
     for file,paths in file_dict.items():
         gt_path,pred_path = paths
@@ -123,8 +126,10 @@ def main(_argv):
             for anno in anno_list:
                 cls,x,y,w,h = anno.split()
                 gt_anno_dict[cls] = gt_anno_dict.get(cls,[])+[list(map(float,[x,y,w,h]))]
+                total_dict['gt'][cls] = total_dict['gt'].get(cls,[])+[list(map(float,[x,y,w,h]))]
                 gt_counter[cls] = gt_counter.get(cls,0)+1
                 gt_cls_count[cls] = gt_cls_count.get(cls,0)+1
+                
 
         pred_anno_dict = {}
         if pred_dict:    
@@ -133,18 +138,21 @@ def main(_argv):
                 for anno in anno_list:
                     if anno=="":
                         continue
-                    cls,x,y,w,h = anno.split()
-                    pred_anno_dict[cls] = pred_anno_dict.get(cls,[])+[list(map(float,[x,y,w,h]))]
+                    cls,score, x,y,w,h = anno.split()
+                    pred_anno_dict[cls] = pred_anno_dict.get(cls,[])+[list(map(float,[score,x,y,w,h]))]
+                    total_dict['pred'][cls] = total_dict['pred'].get(cls,[])+[list(map(float,[x,y,w,h]))]
                     total_tp_dict[cls] = total_tp_dict.get(cls,[])
 
-        total_dict[file] = {'gt':gt_anno_dict,'pred':pred_anno_dict}
-        print(gt_anno_dict)
+        for cls in pred_anno_dict:
+            pred_anno_dict[cls] = sorted(pred_anno_dict[cls],reverse=True)
 
-        # calculate ap per class
+        # calculate map per file
         ap_list = []
         for cls in gt_anno_dict:
             tp = []
+            used_list = []
             for bbox1 in pred_anno_dict.get(cls,[]):
+                score,bbox1 = bbox1[0], bbox1[1:]
                 tmp_list = []
                 for bbox2 in gt_anno_dict.get(cls,[]):
                     iou = cal_iou(bbox1, bbox2)
@@ -153,33 +161,45 @@ def main(_argv):
 
                 if tmp_list:
                     _,now_matched = max(tmp_list)
-                    gt_anno_dict[cls].remove(now_matched)
-                    total_tp_dict[cls].append(1)
-                    tp.append(1)
+                    if now_matched in used_list:
+                        tp.append(0)
+                        total_tp_dict[cls].append([score,0])
+                    else:
+                        used_list.append(now_matched)
+                        tp.append(1)
+                        total_tp_dict[cls].append([score,1])
                 else:
-                    total_tp_dict[cls].append(0)
                     tp.append(0)
+                    total_tp_dict[cls].append([score,0])
 
             ap_list.append(cal_ap(tp, gt_counter[cls]))
-        if ap_list:
-            print(f"{file}\nmAP : {sum(ap_list)/len(ap_list):.2f}")
-        else:
-            print(f"{file}\nmAP : 0")
-            
-    print(gt_cls_count)
-    # total mAP calculate
-    print("\nTotal AP")
+
+        if not FLAGS.summary:
+            if ap_list:
+                print(f"{file}\nmAP : {sum(ap_list)/len(ap_list):.2f}")
+            else:
+                print(f"{file}\nmAP : 0")
+
+        for cls in pred_anno_dict:
+            if cls not in gt_anno_dict:
+                for bbox2 in pred_anno_dict[cls]:
+                    score,_,_,_,_ = bbox2
+                    total_tp_dict[cls].append([score,0])
+                
+    
+    # total mAP calculatea
     ap_dict = {}
     for cls,count in sorted(gt_cls_count.items()):
         tp = total_tp_dict.get(cls,[])
-        print(tp,count)
+        tp = list(map(lambda x:x[1],sorted(tp, reverse=True)))
         ap_dict[cls] = cal_ap(tp,count)
-        print(f"{cls} : {ap_dict[cls]*100:.2f}")
-        
+        if not FLAGS.summary:
+            print(f"{cls} : {ap_dict[cls]*100:.2f}")
 
     print(f"mAP : {sum(ap_dict.values())/len(ap_dict)*100:.2f}")
 
-
+    # print(f"mAP : {sum(ap_list)/len(ap_list)*100:.2f}")
+    # print(len(ap_list))
 
 if __name__=="__main__":
     try:
